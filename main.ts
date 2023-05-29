ee09c8944a7ccd49b0a4274337fe1a419e64ebd7
josemmc06 <106336939+josemmc06@users.noreply.github.com>
2023-05-29 09:01:16 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Ghost)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Umbrella)
+})
